2166d68c0678c97c6882f6ff20bbd0ef9e110090
DouglasAbrams <32776088+DouglasAbrams@users.noreply.github.com>
2019-10-14 13:12:57 -0400

    Add files via upload
    genotyping takes lumpy/destruct vcfs in a path-specifying yaml

--- single_cell/multi_sample.py
+++ single_cell/multi_sample.py
@@ -2,9 +2,10 @@ import os
 
 import pypeliner
 import pypeliner.managed as mgd
-from single_cell import infer_haps
-from single_cell import variant_calling
 from single_cell.utils import helpers
+
+import infer_haps
+import variant_calling
 
 
 def load_cell_data(yamldata, key):
@@ -22,14 +23,24 @@ def load_tumour_data(yamldata):
 
     return sample_data
 
+def load_library_data(yamldata, key, caller):
+    for sample_id, sample_data in yamldata[key].items():
+        for library_id, variant_calls in sample_data.items(): 
+            yield sample_id, library_id, variant_calls[caller]
+
+def load_variant_calls(yamldata, caller):
+    variant_files = {}
+    for sample_id, library_id, variant_call in load_library_data(yamldata, 'variant_calls', caller):
+         variant_files[sample_id, library_id] = variant_call
+    return variant_files
 
 def load_normal_data(yamldata):
     libraries = set()
     if 'normal_wgs' in yamldata:
         assert len(yamldata['normal_wgs'].keys()) == 1
-        sample_id = list(yamldata['normal_wgs'].keys())[0]
+        sample_id = yamldata['normal_wgs'].keys()[0]
         assert len(yamldata['normal_wgs'][sample_id].keys()) == 1
-        library_id = list(yamldata['normal_wgs'][sample_id].keys())[0]
+        library_id = yamldata['normal_wgs'][sample_id].keys()[0]
         libraries.add(library_id)
         cell_bams = yamldata['normal_wgs'][sample_id][library_id]['bam']
     else:
@@ -62,7 +73,8 @@ def generate_meta_files(normal_sample_id, normal_libraries, tumour_cell_bams, ar
     lumpy_dir = args['lumpy_output']
     haps_dir = args['haps_output']
     varcall_dir = args["variants_output"]
-
+    svtyper_dir=args["svtyper_output"]
+    
     tumour_ids = sorted(set([(val[0], val[1]) for val in tumour_cell_bams]))
     tumour_ids = [
         {'sample_id': val[0], 'library_id': val[1]} for val in tumour_ids
@@ -86,7 +98,6 @@ def generate_meta_files(normal_sample_id, normal_libraries, tumour_cell_bams, ar
             destruct_dir,
             filepaths,
             metadata,
-            input_yaml=args['input_yaml']
         )
 
     if lumpy_dir:
@@ -99,9 +110,8 @@ def generate_meta_files(normal_sample_id, normal_libraries, tumour_cell_bams, ar
             lumpy_dir,
             filepaths,
             metadata,
-            input_yaml=args['input_yaml']
         )
-
+    
     if haps_dir:
         haps_files = get_file_paths(haps_dir, 'haps_calling')
         filepaths = [resolve_template(v, tumour_ids) for v in haps_files.values()]
@@ -112,7 +122,6 @@ def generate_meta_files(normal_sample_id, normal_libraries, tumour_cell_bams, ar
             haps_dir,
             filepaths,
             metadata,
-            input_yaml=args['input_yaml']
         )
 
     if varcall_dir:
@@ -125,7 +134,6 @@ def generate_meta_files(normal_sample_id, normal_libraries, tumour_cell_bams, ar
             varcall_dir,
             filepaths,
             metadata,
-            input_yaml=args['input_yaml']
         )
 
 
@@ -134,6 +142,12 @@ def multi_sample_pipeline(args):
 
     tumour_cell_bams = load_tumour_data(data)
     normal_sample_id, normal_libraries, normal_bams = load_normal_data(data)
+
+    #load variant calls
+    if args["variant_calls"]:
+        variant_call_data = helpers.load_yaml(args["variant_calls"])
+        lumpy_variant_calls = load_variant_calls(variant_call_data, "lumpy")
+        destruct_variant_calls = load_variant_calls(variant_call_data, "destruct")
 
     pyp = pypeliner.app.Pypeline(config=args)
 
@@ -145,28 +159,22 @@ def multi_sample_pipeline(args):
         lumpy_dir=args['lumpy_output'],
         haps_dir=args['haps_output'],
         varcall_dir=args["variants_output"],
+        svtyper_dir=args["svtyper_output"],
+        input_lumpy_variant_calls = lumpy_variant_calls,
+        input_destruct_variant_calls = destruct_variant_calls,
         normal_sample_id=normal_sample_id
     )
 
     pyp.run(workflow)
-
     generate_meta_files(normal_sample_id, normal_libraries, tumour_cell_bams, args)
-
-
 def get_file_paths(root_dir, pipeline_type):
     if pipeline_type == 'variant_calling':
         data = {
             'museq_vcfs': os.path.join(root_dir, '{sample_id}_{library_id}_museq.vcf.gz'),
-            'snv_counts': os.path.join(root_dir, '{sample_id}_{library_id}_snv_allele_counts.csv.gz'),
-            'cosmic_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_cosmic_status.csv.gz'),
-            'dbsnp_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_dbsnp_status.csv.gz'),
-            'mappability_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_mappability.csv.gz'),
-            'snpeff_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_snpeff.csv.gz'),
-            'museq_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_museq.csv.gz'),
-            'strelka_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_strelka.csv.gz'),
-            'trinuc_csv': os.path.join(root_dir, '{sample_id}_{library_id}_snv_trinuc.csv.gz'),
-            'strelka_indels': os.path.join(root_dir, '{sample_id}_{library_id}_strelka_indel.vcf.gz'),
             'strelka_snvs': os.path.join(root_dir, '{sample_id}_{library_id}_strelka_snv.vcf.gz'),
+            'strelka_indels': os.path.join(root_dir, '{sample_id}_{library_id}_strelka_indel.vcf.gz'),
+            'snv_annotations': os.path.join(root_dir, '{sample_id}_{library_id}_snv_annotations.h5'),
+            'snv_counts': os.path.join(root_dir, '{sample_id}_{library_id}_snv_counts.h5'),
         }
     elif pipeline_type == 'haps_calling':
         data = {
@@ -212,11 +220,14 @@ def create_multi_sample_workflow(
         lumpy_dir=None,
         haps_dir=None,
         varcall_dir=None,
+        svtyper_dir=None,
+        input_lumpy_variant_calls = None,
+        input_destruct_variant_calls = None,
         normal_sample_id='normal',
 ):
     """ Multiple sample pseudobulk workflow. """
 
-    baseimage = config['multi_sample']['docker']['single_cell_pipeline']
+    baseimage = config['genotyping']['docker']['scp']
     ctx = {'mem_retry_increment': 2, 'disk_retry_increment': 50, 'ncpus': 1, 'docker_image': baseimage}
 
     workflow = pypeliner.workflow.Workflow(default_ctx=ctx)
@@ -253,21 +264,9 @@ def create_multi_sample_workflow(
                                template=varcall_files['strelka_snvs']),
                 mgd.OutputFile('strelka_indel.vcf', 'sample_id', 'library_id', axes_origin=[],
                                template=varcall_files['strelka_indels']),
-                mgd.OutputFile('museq_snv.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['museq_csv']),
-                mgd.OutputFile('strelka_snv.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['strelka_csv']),
-                mgd.OutputFile('cosmic.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['cosmic_csv']),
-                mgd.OutputFile('dbsnp.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['dbsnp_csv']),
-                mgd.OutputFile('mappability.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['mappability_csv']),
-                mgd.OutputFile('snpeff.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['snpeff_csv']),
-                mgd.OutputFile('trinuc.csv.gz', 'sample_id', 'library_id', axes_origin=[],
-                               template=varcall_files['trinuc_csv']),
-                mgd.OutputFile('snv_counts.csv.gz', 'sample_id', 'library_id', axes_origin=[],
+                mgd.OutputFile('snv_annotations.h5', 'sample_id', 'library_id', axes_origin=[],
+                               template=varcall_files['snv_annotations']),
+                mgd.OutputFile('snv_counts.h5', 'sample_id', 'library_id', axes_origin=[],
                                template=varcall_files['snv_counts']),
             )
         )
@@ -354,7 +353,7 @@ def create_multi_sample_workflow(
 
         workflow.subworkflow(
             name='run_lumpy_multi_sample',
-            func='single_cell.workflows.lumpy.lumpy_multi_sample_workflow',
+            func='single_cell.workflows.lumpy.lumpy_multi_sample_workflow2',
             ctx={'docker_image': lumpy_pipeline_config['docker']['single_cell_pipeline']},
             args=(
                 lumpy_pipeline_config,
@@ -366,5 +365,53 @@ def create_multi_sample_workflow(
                 mgd.OutputFile('lumpy_breakpoints.bed', 'sample_id', 'library_id', axes_origin=[]),
             )
         )
+    
+    #performs genotyping on lumpy + destruct variant calls
+    if svtyper_dir and input_lumpy_variant_calls and input_destruct_variant_calls:
+        genotyping_pipeline_config = config['genotyping']
+        output_template = {}
+        
+        annotations = ["AO", "AP", "AS",
+                       "ASC","DP", "GQ",
+                       "QA", "QR", "RO",
+                       "RP", "RS", "SQ",
+                       "GL", "AB"]
+
+        for annotation in annotations: 
+            output_template[annotation] =  os.path.join(svtyper_dir, '{sample_id}', '{library_id}', annotation+".csv")
+
+        output_template["merged"] = os.path.join( svtyper_dir, '{sample_id}', '{library_id}', 'lumpy_destruct_merged.csv')
+        ref = genotyping_pipeline_config["data"]["reference"]
+        workflow.subworkflow(
+            name='genotype',
+            func='single_cell.workflows.genotyping.genotyping',
+            axes=('sample_id', 'library_id', ),
+            ctx={'docker_image': genotyping_pipeline_config['docker']['scp']},
+            args=(
+                ref,
+                mgd.InputFile('bam_input', 'sample_id', 'library_id',  'cell_id', extensions=['.bai'],
+                              axes_origin=[], fnames=tumour_cell_bams),
+                mgd.InputFile('lumpy_csv_gz', 'sample_id', 'library_id', axes_origin=[], fnames=input_lumpy_variant_calls),
+                mgd.InputFile('destruct_csv_gz', 'sample_id', 'library_id', axes_origin=[], fnames=input_destruct_variant_calls),
+                mgd.OutputFile('AO', 'sample_id', 'library_id', template=output_template["AO"]),                                          
+                mgd.OutputFile('AP', 'sample_id', 'library_id', template=output_template["AP"]),
+                mgd.OutputFile('AS', 'sample_id', 'library_id', template=output_template["AS"]),
+                mgd.OutputFile('ASC', 'sample_id', 'library_id', template=output_template["ASC"]),
+                mgd.OutputFile('DP', 'sample_id', 'library_id', template=output_template["DP"]),
+                mgd.OutputFile('GQ', 'sample_id', 'library_id', template=output_template["GQ"]),
+                mgd.OutputFile('QA', 'sample_id', 'library_id', template=output_template["QA"]),
+                mgd.OutputFile('QR', 'sample_id', 'library_id', template=output_template["QR"]),
+                mgd.OutputFile('RO', 'sample_id', 'library_id', template=output_template["RO"]),
+                mgd.OutputFile('RP', 'sample_id', 'library_id', template=output_template["RP"]),
+                mgd.OutputFile('RS', 'sample_id', 'library_id', template=output_template["RS"]),
+                mgd.OutputFile('SQ', 'sample_id', 'library_id', template=output_template["SQ"]),
+                mgd.OutputFile('GL', 'sample_id', 'library_id', template=output_template["GL"]),
+                mgd.OutputFile('AB', 'sample_id', 'library_id', template=output_template["AB"]),
+                genotyping_pipeline_config
+            )
+        )
 
     return workflow
+    
+    
+    
